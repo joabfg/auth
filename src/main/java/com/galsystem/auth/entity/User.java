4164f661290a145fa9a293c1faab40efe3809da2
Joab Galdino <joabfgal@gmail.com>
2020-12-08 22:29:38 -0300

    class user

---
+++ src/main/java/com/galsystem/auth/entity/User.java
@@ -0,0 +1,83 @@
+package com.galsystem.auth.entity;
+
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "permission")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User implements Serializable, UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name",nullable = false)
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "accountNonExpired")
+    private Boolean accountNonExpired;
+
+    @Column(name = "accountNonLocked")
+    private Boolean accountNonLocked;
+
+    @Column(name = "credentialsNonExpired")
+    private Boolean credentialsNonExpired;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission", joinColumns = {@JoinColumn(name = "id_user")},
+    inverseJoinColumns =  {@JoinColumn(name = "id_permissions")})
+    private List<Permission> permissions;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.permissions;
+    }
+
+    public List<String> getRoles(){
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
