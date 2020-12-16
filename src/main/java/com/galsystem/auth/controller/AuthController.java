78e6392b8bab10da0225e42b011fb4977fadd978
Joab Galdino <joabfgal@gmail.com>
2020-12-16 15:23:37 -0300

    class autentication

--- src/main/java/com/galsystem/auth/controller/AuthController.java
+++ src/main/java/com/galsystem/auth/controller/AuthController.java
@@ -35,7 +35,7 @@ public class AuthController {
         this.userRepository = userRepository;
     }
 
-    @RequestMapping("testeSecurity")
+    @RequestMapping("/testeSecurity")
     public String teste(){
         return "testado";
     }
